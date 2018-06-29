fd8a238e87c52f95dcf7fe226ede74fafc19f285
Hernaldo Henriquez <hjhenriq@gmail.com>
2018-06-29 15:07:01 -0400

    update store and filtering sections

--- src/components/App.js
+++ src/components/App.js
@@ -4,21 +4,23 @@ import CreateLink from './CreateLink'
 import LinkList from './LinkList'
 import Login from './Login'
 import Header from './Header'
+import Search from './Search'
 
 class App extends Component {
   render() {
     return (
-      <div className="center w85">
+      <div className='center w85'>
         <Header />
-        <div className="ph3 pv1 background-gray">
+        <div className='ph3 pv1 background-gray'>
           <Switch>
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/" component={LinkList} />
-            <Route exact path="/create" component={CreateLink} />
+            <Route exact path='/' component={LinkList}/>
+            <Route exact path='/create' component={CreateLink}/>
+            <Route exact path='/login' component={Login}/>
+            <Route exact path='/search' component={Search}/>
           </Switch>
         </div>
       </div>
-    );
+    )
   }
 }
 
